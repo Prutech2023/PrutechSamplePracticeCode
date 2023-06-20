58504baeb93907f583bea783518c33137c2f1b00
Prutech2023 <dineshprutech@gmail.com>
2023-06-20 11:13:34 +0530

    Added user class

---
+++ Prutech.Examples.API/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Prutech.Examples.API.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string DateOfBirth { get; set; }
+
+    public string Gender { get; set; }
+
+    public string CurrentAddress { get; set; }
+
+    public string PermanentAddress { get; set; }
+
+    public string City { get; set; }
+
+    public string Nationality { get; set; }
+
+    public string PINCode { get; set; }
+}
