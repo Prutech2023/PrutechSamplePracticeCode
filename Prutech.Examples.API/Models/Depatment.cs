08723b42159b356a2a60f9632103e21ea7ae1161
dineshprutech@gmail.com <dineshprutech@gmail.com>
2023-06-20 12:02:56 +0530

    Added created by property

--- Prutech.Examples.API/Models/Depatment.cs
+++ Prutech.Examples.API/Models/Depatment.cs
@@ -2,4 +2,8 @@
 
 public class Depatment
 {
+    public int DepartmentId { get; set; }
+    public string DepartmentName { get; set; }
+    public bool IsActive { get; set; }
+    public int CreatedBy { get; set; }
 }
