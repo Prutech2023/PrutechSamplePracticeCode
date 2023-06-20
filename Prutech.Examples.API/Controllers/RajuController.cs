f73f36c05002231b69a2654b99956d158b79fb02
Prutech2023 <dineshprutech@gmail.com>
2023-06-20 07:54:49 +0530

    Initial code check in adding projects
    Initial code check in

---
+++ Prutech.Examples.API/Controllers/RajuController.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Prutech.Examples.API.Models;
+
+namespace Prutech.Examples.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RajuController : ControllerBase
+{
+ 
+   
+    [HttpGet]
+    public string AddTwoNumbers(int a , int b)
+    {
+        Employee employee = new Employee();
+        employee.EmployeeId = 1;
+        return $" this is the calcuation of A = {a}  and B = {b} after adding both the values  are  : {(a + b)}"; 
+    }
+
+}
